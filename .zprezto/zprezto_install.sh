dbaf9c8411f3774dea5eafa8e56e44c4c13d15b4
仲村 <>
2019-11-12 20:28:46 +0900

    correct a typo

--- .zprezto/zprezto_install.sh
+++ .zprezto/zprezto_install.sh
@@ -1,4 +1,4 @@
-etopt EXTENDED_GLOB
+setopt EXTENDED_GLOB
 ZPREZTODIR=$HOME/dotfiles
 for rcfile in "${ZPREZTODIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
