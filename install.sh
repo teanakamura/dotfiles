5d9aedbe4837cf3c7c8181f2895eacbf55b140a5
仲村 <>
2019-11-13 20:58:09 +0900

    change file structure

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-DOTPATH=~/dotfiles
+DOTPATH=~/dotfiles/.dotfiles
 
-for f in .??*
+cd .dotfiles
+for f in ?*
 do
-  [[ "$f" == ".zprezto" ]] && continue
-  [[ "$f" == ".git" ]] && continue
+  [[ "$f" == "git" ]] && continue
   [[ "$f" == ".DS_Store" ]] && continue
 
-  echo "$f"
-  ln -snfv "$DOTPATH/$f" "$HOME"/"$f"
+  echo ".$f"
+  ln -snfv "$DOTPATH/$f" "$HOME/.$f" 
 done
+cd ..
