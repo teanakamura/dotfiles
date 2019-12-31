42c3e22f6fd91e299dff2ff6e7ccb024e113ccab
仲村 <>
2019-12-31 15:09:48 +0900

    rename zprezto dir

--- zprezto/zprezto_install.sh
+++ zprezto/zprezto_install.sh
@@ -1,5 +1,5 @@
 setopt EXTENDED_GLOB
 ZPREZTODIR=$HOME/dotfiles
-for rcfile in "${ZPREZTODIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+for rcfile in "${ZPREZTODIR:-$HOME}"/zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
