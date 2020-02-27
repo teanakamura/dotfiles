765f71b804dfece69d5011df0c815d0db7542ed4
仲村 <>
2020-02-27 23:56:58 +0900

    modify fzf install script

--- cli/cli_installers/install_fzf.sh
+++ cli/cli_installers/install_fzf.sh
@@ -9,6 +9,7 @@ if [[ ! -e fzf ]]; then
   git clone -depth 1 https://github.com/junegunn/fzf.git
   cd ./fzf/install
   echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' >> ~/.local_conf/localrc
+  echo "which ag && export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g \"\"'" >> ~/.fzf.zsh
   #ln -s ~/.local/share/fzf/bin/fzf ~/.local/bin
   #ln -s ~/.local/share/fzf/bin/fzf-tmux ~/.local/bin
   ln -s ~/dotfiles/cli/cli_bin/fv ~/.local/bin
