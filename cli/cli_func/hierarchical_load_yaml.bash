97d52b91cdf507591423c23cba1c7ecde736464d
teanakamura <wayward.at23@gmail.com>
2020-12-07 14:00:37 +0900

    fix a bug in h_load_yaml func

--- cli/cli_func/hierarchical_load_yaml.bash
+++ cli/cli_func/hierarchical_load_yaml.bash
@@ -4,17 +4,18 @@ function h_load_yaml () {
   ROOT_PATH=$1
   source ~/dotfiles/cli/cli_func/pathlib.bash
   str_split $(path_get_relative $ROOT_PATH $2) '/' 'REL_PATH_ARR'
+  echo "YAML LOADING PATH ARRAY: ${REL_PATH_ARR[@]}"
 
   yml_path=$ROOT_PATH
   i=0
   while [ $i -lt ${#REL_PATH_ARR[@]} ]; do
     if [ -f "${yml_path}/default.yml" ]; then
-      echo "${yml_path}/default.yml"
-      eval $(parse_yaml ${yml_path/default.yml})
+      echo "LOAD ${yml_path}/default.yml"
+      eval $(parse_yaml "${yml_path}/default.yml")
     fi
     yml_path="${yml_path}/${REL_PATH_ARR[$i]}"
     i=`expr $i + 1`
   done
-  echo $yml_path
+  echo "LOAD $yml_path"
   eval $(parse_yaml ${yml_path/default.yml})
 }
